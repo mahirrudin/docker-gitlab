93d856bb09cc8348030e53c468f4c4286d81b325
mahirrudin <mahirrudin@gmail.com>
2020-01-20 21:58:59 +0700

    Update runner-register.sh

--- script/runner-register.sh
+++ script/runner-register.sh
@@ -9,7 +9,7 @@ docker exec -it gitlab-runner \
     --registration-token ${registration_token} \
     --locked=false \
     --description docker-stable \
-    --url http://gitlab-web \
+    --url http://gitlab.devsecops.lab \
     --executor docker \
     --docker-image docker:stable \
     --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
