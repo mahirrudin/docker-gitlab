1b4e836feb3801b9d41d648f5dd906b2b765794a
mahirrudin <mahirrudin@gmail.com>
2020-01-21 09:51:37 +0700

    Update runner-register.bat

--- script/runner-register.bat
+++ script/runner-register.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p registrationtoken="Enter Registration Token: "
 
-docker exec -it gitlab-runner gitlab-runner register --non-interactive --registration-token "%registrationtoken%" --locked=false --description docker-stable --url http://gitlab.devsecops.lab --executor docker --docker-image docker:stable --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" --docker-network-mode gitlab-network
+docker exec -it runner.devsecops.lab gitlab-runner register --non-interactive --registration-token "%registrationtoken%" --locked=false --description docker-stable --url http://gitlab.devsecops.lab --executor docker --docker-image docker:stable --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" --docker-network-mode gitlab-network
